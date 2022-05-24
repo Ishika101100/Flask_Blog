b767e971adff63e0d7e99c0b52b0dac005d196e8
Ishika Shah <ishika.inexture@gmail.com>
2022-05-24 10:32:53 +0530

    intial commit

---
+++ flaskblog/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+# from flask import Flask
+# import os
+# from dotenv import dotenv_values
+load_dotenv()
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
